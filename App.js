a6488cf8468899e927582d81f4f7b6b92a32e174
workpebojot <pebojot.edison.work@gmail.com>
2020-11-28 00:24:02 +0800

    Feature/App.js/Modified-file-for-native-navigation-in-android

---
+++ App.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import * as React from 'react';
+import { Text } from 'react-native';
+
+export default function App() {
+  return <Text>Hello World!</Text>
+}
