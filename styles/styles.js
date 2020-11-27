9ef80b51bb8ba08c7ce3948a44233bf65730d6b5
workpebojot <pebojot.edison.work@gmail.com>
2020-11-28 00:12:32 +0800

    feature/styles/styles-for-flexbox

---
+++ styles/styles.js
@@ -0,0 +1,43 @@
+import { Platform, StyleSheet, StatusBar } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-around",
+        padding: 20,
+        backgroundColor: "ghostwhite",
+        ...Platform.select({
+            ios: { paddingTop: 20 },
+            android: { paddingTop: StatusBar.currentHeight }
+        })
+    },
+    box: {
+        height: 100,
+        width: 100,
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth: 1,
+        borderStyle: "dashed",
+        borderColor: "darkslategray",
+        backgroundColor: "lightgray",
+    },
+    boxText: {
+        color: "darkslategray",
+        fontWeight: "bold"
+    },
+    row: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignSelf: "stretch"
+    },
+    column: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-around",
+        alignSelf: "stretch"
+    }
+});
