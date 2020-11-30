af7aad27bb310c6c0ef01ab032ac9ccc46eeaf78
workpebojot <pebojot.edison.work@gmail.com>
2020-11-30 19:47:28 +0800

    Feature/SplashScreen/New-package-for-splashscreen

--- android/app/src/main/java/com/vacuum/MainActivity.java
+++ android/app/src/main/java/com/vacuum/MainActivity.java
@@ -1,8 +1,14 @@
 package com.vacuum;
 
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
+
 import com.reactnativenavigation.NavigationActivity;
 
 public class MainActivity extends NavigationActivity {
-
-  
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, R.style.SplashScreenTheme);
+        super.onCreate(savedInstanceState);
+    }
 }
