085347495dcff6ba971fd1c240c465ce8aa8c57d
workpebojot <pebojot.edison.work@gmail.com>
2020-12-06 23:40:10 +0800

    Feature/Android/Lottie-Animation

--- android/app/src/main/java/com/vacuum/MainApplication.java
+++ android/app/src/main/java/com/vacuum/MainApplication.java
@@ -13,6 +13,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.aakashns.reactnativedialogs.ReactNativeDialogsPackage;
 import androidx.multidex.MultiDex;
+import com.airbnb.android.react.lottie.LottiePackage;
 
 public class MainApplication extends NavigationApplication {
 
@@ -35,6 +36,7 @@ public class MainApplication extends NavigationApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new LottiePackage());
           new ReactNativeDialogsPackage();
           return packages;
         }
