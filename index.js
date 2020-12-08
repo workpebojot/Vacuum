e3135281bb071efa95862e8536997bc9e6bf2c73
workpebojot <pebojot.edison.work@gmail.com>
2020-12-08 21:38:02 +0800

    Feature/Find-Job/Page-for-finding-job

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ import SignUp from './src/signup/SignUp';
 import Login from './src/login/Login';
 import SetupProfile from './src/setup-profile/SetupProfile';
 import SelectTask from './src/select-task/SelectTask';
+import FindJob from './src/find-job/FindJob';
 
 Navigation.registerComponent('OverView', () => OverView);
 Navigation.registerComponent('SignUp', () => SignUp);
 Navigation.registerComponent('Login', () => Login);
 Navigation.registerComponent('SetupProfile', () => SetupProfile);
 Navigation.registerComponent('SelectTask', () => SelectTask);
+Navigation.registerComponent('FindJob', () => FindJob);
 
 Navigation.events().registerAppLaunchedListener(async () => {
     Navigation.setRoot({
