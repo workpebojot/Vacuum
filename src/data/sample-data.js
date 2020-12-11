3c0650d4a133d53d7ac2d692b396340b09d2108f
workpebojot <pebojot.edison.work@gmail.com>
2020-12-11 14:04:23 +0800

    Update Sample Data
    Update **name**, **author**, **reward** and **cleaner** property value in Sample Data Object. For *name*, remove the room, for *author*, make it long, for *reward*, make it double(number), align the *cleaner* property for new property. This makes the data model more concise and reusable.
    
    Further information for update sample data:
    
    * update object propery

--- src/data/sample-data.js
+++ src/data/sample-data.js
@@ -1,33 +1,54 @@
 const SampleData = [
     {
-        name: "Kitchen Room",
-        author: "Pebojot",
+        id: 0,
+        image: require('.././assets/images/1-demo-user.png'),
+        name: "Kitchen",
+        author: {
+            first: "Edison",
+            last: "Pebojot"
+        },
         date: "January 1, 2021",
         day: "Friday",
         hours: 2,
         location: "Paltic",
-        reward: 200,
-        cleaner: 21
+        reward: 200.00,
+        cleaner: 21,
+        description: "As a cleaner, you will work in kitchen",
+        task: ["Wash and put away dishes", "Wipe down cabinets", "Organize drawers"]
     },
     {
-        name: "Living Room",
-        author: "James",
+        id: 1,
+        image: require('.././assets/images/3-demo-user.png'),
+        name: "Living",
+        author: {
+            first: "James",
+            last: "Gem"
+        },
         date: "January 2, 2022",
         day: "Saturday",
         hours: 3,
         location: "Umiray",
-        reward: 800,
-        cleaner: 26
+        reward: 100,
+        cleaner: 26,
+        description: "Hi, do you like to work in the living room. Please join us.",
+        task: ["Dust shelves", "Dust tabletops", "Dust electronics", "Vacuum drapes w/ attachment"]
     },
     {
-        name: "Wash Room",
-        author: "Jasmine",
+        id: 2,
+        image: require('.././assets/images/2-demo-user.png'),
+        name: "Wash",
+        author: {
+            first: "Jasmine",
+            last: "Ricc"
+        },
         date: "January 3, 2021",
         day: "Sunday",
         hours: 3,
         location: "Matawe",
         reward: 100,
-        cleaner: 18
+        cleaner: 18,
+        description: "Hello! I am jasmine. Working in the laundry made easy.",
+        task: ["Empty Trash", "Run cleaning cycle in washing machine", "Clean out dryer", "Reorganize supplies"]
     }
 ]
 
