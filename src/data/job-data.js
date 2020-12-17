1c8107ce87b2a12ba1fcfbd7fb59503df9433a28
workpebojot <pebojot.edison.work@gmail.com>
2020-12-17 12:29:53 +0800

    Add New Job Data
    See: #7

---
+++ src/data/job-data.js
@@ -0,0 +1,15 @@
+const Job = [
+    "Select Work",
+    "Kitchen",
+    "Living",
+    "Wash",
+    "Bath",
+    "Bed",
+    "Garage",
+    "Closets",
+    "Dining",
+    "Office",
+    "Throughout House"
+]
+
+export default Job;
