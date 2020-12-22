d153dd25c55ed687f6cf5dbba63aec323d2c34c3
workpebojot <pebojot.edison.work@gmail.com>
2020-12-23 00:19:24 +0800

    Modify and delete files
    See: #25

--- src/data/task/task-closets-data.js
+++
@@ -1,8 +0,0 @@
-const TaskClosetsData = [
-    { label: "Sort clothes to keep / donate /throw out" },
-    { label: "Take dry cleaning and / or clothes to be tailored " },
-    { label: "Organize items " },
-    { label: "Dust shelves" }
-]
-
-export default TaskClosetsData;
--- src/data/task/task-entry-data.js
+++
@@ -1,7 +0,0 @@
-const TaskEntryData = [
-    { label: "Dust furniture" },
-    { label: "Clean rug / welcome mat" },
-    { label: "Organize shoes / coats" }
-]
-
-export default TaskEntryData;
